65cb28aaf4d899964cbb5676bb26c2dbeca33eba
Edward Wang <edawordwang@gmail.com>
2026-06-16 01:56:12 -0400

    print

--- plugin/hello.py
+++ plugin/hello.py
@@ -1,4 +1,5 @@
 from kipy import KiCad
+from kipy.geometry import Vector2, Angle
 
 if __name__ == "__main__":
     try:
@@ -6,3 +7,9 @@ if __name__ == "__main__":
         print(f"Connected to KiCad {kicad.get_version()}")
     except BaseException as e:
         print(f"Not connected to KiCad: {e}")
+
+    board = kicad.get_board()
+    footprints = board.get_footprints()
+
+    for f in footprints:
+        print(f"{f.reference_field.text.value}")
